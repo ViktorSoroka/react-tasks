f61b9e4b8779fd547e2791560dfc2477dca05022
Viktor Soroka <viktor_soroka@epam.com>
2016-11-25 18:36:03 +0200

    Create react base structure.

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import logo from '../../logo.svg';
+import './App.css';
+
+export default App => (
+    <div className="app">
+        <div className="app-header">
+            <img src={logo} className="app-header__logo" alt="logo"/>
+            <h2>Welcome to React</h2>
+        </div>
+        Let`s go
+    </div>
+);
